f7948036c7f31535e897b18d9d1c76d8f7a8ae2e
Manel <92manel@gmail.com>
2016-06-18 17:02:38 +0200

    Implementation of Neural Network and tunning its parameters

--- final_project/yeast_classification.R
+++ final_project/yeast_classification.R
@@ -3,6 +3,8 @@
 
 # Description of the variables
 #http://archive.ics.uci.edu/ml/machine-learning-databases/yeast/yeast.names
+# Delete all variables
+# rm(list=ls(all=TRUE))
 
 # So as that the same results could be obtained again 
 set.seed(777)
@@ -61,9 +63,8 @@ N <- dim(yeast)[1]
 learn <- sample(1:N, round(2/3*N))
 yeast.train <- yeast[learn,]
 yeast.test <- yeast[-learn,]
+dim(yeast.train)
 dim(yeast.test)
-yeast[row.names(yeast[-learn,]),]
-match(row.names(yeast),row.names(yeast.test))
 
 library(TunePareto)
 # Prepare a crossvalidation 10x10 method to get the best model with
@@ -112,13 +113,17 @@ for (j in 1:k) {
 }
 cv.results
 # Average of the validation error
+
 (VA.error <- mean(cv.results[,"VA error"]))
-# library necessary for Naive Bayes
+# necessary library for Naive Bayes
 library(e1071)
-# library necessary for Random Forest
+# necessary library for Random Forest
 library(randomForest)
-# library necessary for LDA and QDA
+# necessary library for LDA and QDA
 library(MASS)
+# necessary library for Neural Networks
+library(nnet)
+
 k <- 10
 CV.folds <- generateCVRuns(yeast.train$class, ntimes=1, nfold=k, stratified=TRUE)
 Model.CV <- function (method)
@@ -146,7 +151,7 @@ Model.CV <- function (method)
       cv.results[j,"fold"] <- j
     }
     else if (method == "QDA"){
-      model <- lda(tr$class ~ . , data = tr, CV=FALSE)
+      model <- qda(tr$class ~ . , data = tr, CV=FALSE)
       # predict TR data
       pred.tr <- predict(model)$class
       tab <- table(tr$class, pred.tr)
@@ -178,7 +183,9 @@ Model.CV <- function (method)
       stop("Unknown method. The only valid methods ara NaiveBayes, LDA, QDA and RandomForest")
     }
   }
-  mean(cv.results[,"VA error"])
+  
+  return(list(cv.results, mean(cv.results[,"VA error"])))
+  # mean(cv.results[,"VA error"])
 }
 # Test the behavior for Naive Bayes
 Model.CV("NaiveBayes")
@@ -211,7 +218,6 @@ pca.yeast$eig
 # keep 5 eigenvalues (which is the default number in PCA))
 # Now we can train a neural network
 pca.yeast.train <- pca.yeast$ind$coord
-library(nnet)
 summary(yeast)
 
 
@@ -225,17 +231,78 @@ summary(yeast)
 # Naive Bayes
 
 yeast.nb <- naiveBayes (yeast.train$class ~ ., data=yeast.train, laplace=3)
-# predict the left-out data
+# predict the test data
 pred <- predict(yeast.nb,newdata=yeast.test)
 (tt <- table(Truth=yeast.test$class, Predicted=pred))
 (error <- 100*(1-sum(diag(tt))/sum(tt)))
 # Reduction of the error
 100*(baseline-error)/baseline
 # Real error for each class
-for (i in 1:8) {
+for (i in 1:length(levels(yeast$class))) {
   cat(levels(yeast.test$class)[i])
   print(1- tt[i,i]/sum(tt[i,]))
 }
+
+# LDA 
+
+model <- lda(yeast.train$class ~ . , data = yeast.train, CV=FALSE)
+# predict Test data
+pred <- predict(model, newdata = yeast.test)$class
+(tt <- table(Truth=yeast.test$class, Predicted=pred))
+(error <- 100*(1-sum(diag(tt))/sum(tt)))
+# Reduction of the error
+100*(baseline-error)/baseline
+# Real error for each class
+for (i in 1:length(levels(yeast$class))) {
+  cat(levels(yeast.test$class)[i])
+  print(1- tt[i,i]/sum(tt[i,]))
+}
+
+# QDA
+# Only works after feature selection, due to the fact that some modalities
+# of yeast$class are too rare or appear so few times
+
+model <- qda(yeast.train$class ~ . , data = yeast.train, CV=FALSE)
+# predict TR data
+pred <- predict(model, yeast.test)$class
+(tt <- table(Truth=yeast.test$class, Predicted=pred))
+(error <- 100*(1-sum(diag(tt))/sum(tt)))
+# Reduction of the error
+100*(baseline-error)/baseline
+# Real error for each class
+for (i in 1:length(levels(yeast$class))) {
+  cat(levels(yeast.test$class)[i])
+  print(1- tt[i,i]/sum(tt[i,]))
+}
+
+
+# KNN
+
+
+# Neural Networks
+
+model.nnet <- nnet(class ~., data = yeast, subset=learn, size=20, maxit=200, decay=0.01)
+summary(model.nnet)
+# TR error
+p1 <- as.factor(predict (model.nnet, type="class"))
+t1 <- table(p1,yeast.train$class)
+(error_rate.learn <- 100*(1-sum(diag(t1))/dim(yeast.train)[1]))
+# TEST error
+p2 <- as.factor(predict (model.nnet, newdata=yeast.test, type="class"))
+t2 <- table(p2,yeast.test$class)
+(error_rate.test <- 100*(1-sum(diag(t2))/dim(yeast.test)[1]))
+library(caret)
+trc <- trainControl (method="repeatedcv", number=10, repeats=10)
+decays <- 10^seq(-3,0,by=0.2)
+model.10x10CV <- train (class ~., data = yeast, subset=learn, method='nnet', maxit = 200, trace = FALSE,
+                        tuneGrid = expand.grid(.size=20,.decay=decays), trControl=trc)
+## We can inspect the full results
+model.10x10CV$results
+## and the best model found
+model.10x10CV$bestTune
+# Fit the best model with all the training and check the results: 
+model.nnet <- nnet(class ~., data = yeast, subset=learn, size=20, maxit=200, decay=0.01)
+
 
 # Random Forest
 
